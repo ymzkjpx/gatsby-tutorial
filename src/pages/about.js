9a5f66fcfb6dae0a68834809696fe49d89e6ccd8
ymzkjpx <ymzkjpx@gmail.com>
2021-10-15 02:43:38 +0900

    tuto2-Link

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () =>{
+    return(
+        <main>
+            <title>About me</title>
+            <h1>About Me</h1>
+            <p>Hi there!</p>
+        </main>
+    )
+}
+export default AboutPage
