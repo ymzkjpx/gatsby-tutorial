395d2ff11e0657a7ae8b4323836341afdd9babcd
ymzkjpx <ymzkjpx@gmail.com>
2021-10-24 19:31:51 +0900

    GatsbyImageを追加

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,26 @@
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/layout'
 
+
 const BlogPost=({data})=>{
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
     return(
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+              image={image}
+              alt={data.mdx.frontmatter.hero_image_alt}
+            />
+            <p>
+              Photo Credit:{" "}
+              <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                {data.mdx.frontmatter.hero_image_credit_text}
+              </a>
+            </p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -16,14 +30,26 @@ const BlogPost=({data})=>{
 
 export const query = graphql`
 query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        date(formatString: "YYYY-MM-DD")
-        title
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      date(formatString: "YYYY-MM-DD")
+      title
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image {
+        childImageSharp {
+          gatsbyImageData(
+            blurredOptions: {toFormat: AUTO}
+            layout: CONSTRAINED
+            width: 1080
+          )
+        }
       }
-      body
     }
-  }  
+    body
+  }
+}
 `
 
 export default BlogPost
